0717c62e44a98aac38ccac153b5406b3d46c81ae
Victor-MacielMF <joaovictormacieldefreitas@gmail.com>
2025-06-08 16:48:04 -0300

    feat: created test project with xUnit and fluent assertions for basic service methods

---
+++ NetworkUtility.Tests/PingTest/NetworkServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+
+namespace NetworkUtility.Tests.PingTest
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NeworkService_SendPing_ReturnString()
+        {
+            NetworkService pingService = new NetworkService();
+            string result;
+
+            result = pingService.SendPing();
+
+            result.Should().NotBeNullOrEmpty();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int output)
+        {
+            NetworkService pingService = new NetworkService();
+            int result;
+
+            result = pingService.PingTimeout(a, b);
+            result.Should().Be(output);
+        }
+    }
+}
